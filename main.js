cce76ff2b6f3d123ac4b899e33d428a2a8c08dd6
Jesse Soldat <jsoldat@hotmail.com>
2016-01-10 10:22:05 -0500

    clone a new copy of the original array on each loop and randomly splice out a letter each time we run the loop based on the original array's length

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
+//Random Section
 var random = document.getElementById('random');
-var sorted = document.getElementById('sorted');
+
 
 var myArray = ['A','B','C','D','E','F','G'];
 var newArray =  [];
@@ -12,30 +13,57 @@ for(i = 0; i < 50; i++) {
   random.innerHTML += myArray[randomIndex];  
   newArray.push(myArray[randomIndex]);
 }
+//Sort Section
+var sorted = document.getElementById('sorted');
 
- newArray.sort();
- text = newArray.join('');
+newArray.sort();
+text = newArray.join('');
 
- sorted.innerHTML = text;
+sorted.innerHTML = text;
 
- var a = text.match(/A/gi).length;
- var b = text.match(/B/gi).length;
- var c = text.match(/C/gi).length;
- var d = text.match(/D/gi).length;
- var e = text.match(/E/gi).length;
- var f = text.match(/F/gi).length;
- var g = text.match(/G/gi).length;
+//Occurrence Section  
+var a = text.match(/A/gi).length;
+var b = text.match(/B/gi).length;
+var c = text.match(/C/gi).length;
+var d = text.match(/D/gi).length;
+var e = text.match(/E/gi).length;
+var f = text.match(/F/gi).length;
+var g = text.match(/G/gi).length;
 
- var amount = [a,b,c,d,e,f,g];
+var amount = [a,b,c,d,e,f,g];
 
 
- letters = document.getElementById('letters');
+letters = document.getElementById('letters');
 
- for(i = 0; i < amount.length; i++) {
+for(i = 0; i < amount.length; i++) {
   var li = document.createElement("li");
   li.appendChild(document.createTextNode(myArray[i]+': '+amount[i]));
   letters.appendChild(li);
  }
+
+//Spice Section - each time clone a new copy of our original array and splice out a random letter to display
+var splice = document.getElementById('splice');
+
+var times = ['1st: ','2nd: ','3rd: ','4th: ','5th: ','6th: ','7th: ']
+
+
+for(i = 0; i < myArray.length; i++) {
+    var clone = [];
+    for(int = 0; int < myArray.length; int++) {
+      clone.push(myArray[int]);
+    }
+
+    randomIndex = Math.floor(Math.random() * clone.length);
+    var randomSplice = clone.splice(randomIndex, 1);
+    // console.log(times[i]+randomSplice);
+    var list = document.createElement("li");
+    list.appendChild(document.createTextNode(times[i]+randomSplice));
+    splice.appendChild(list);
+}
+
+//Remove one Letter randomly at a time until we are left with only one
+
+
 
 
 
